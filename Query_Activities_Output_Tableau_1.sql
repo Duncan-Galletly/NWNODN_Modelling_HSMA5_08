a5ca54cdfc3399497f8f1fe27c7fa5195ab08840
Passey Duncan (NHS North of England Commissioning Support Unit) <Duncan.Passey@NECSU.informatix.loc>
2023-11-30 10:37:44 +0000

    Summary SQL update for data loading and year dates error fix
    Year dates a loading data tweak

--- Query_Activities_Output_Tableau_1.sql
+++ Query_Activities_Output_Tableau_1.sql
@@ -3,7 +3,8 @@ DECLARE @DynamicUnpivotQuery NVARCHAR(MAX),
         @SelectSolutionNumbers NVARCHAR(MAX)
 
 -- Generate the list of solution columns for unpivoting
-SELECT @SolutionColumns = STUFF((SELECT ', ' + QUOTENAME(COLUMN_NAME)
+SELECT @SolutionColumns = 
+	STUFF((SELECT ', ' + QUOTENAME(COLUMN_NAME)
     FROM INFORMATION_SCHEMA.COLUMNS
     WHERE TABLE_SCHEMA = 'dbo'
     AND TABLE_NAME = 'NICU_Modelling_Activities_Output'
@@ -11,7 +12,8 @@ SELECT @SolutionColumns = STUFF((SELECT ', ' + QUOTENAME(COLUMN_NAME)
     FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 2, '')
 
 -- Generate the SELECT statement part for solution numbers dynamically
-SELECT @SelectSolutionNumbers = STUFF((SELECT ', '''+ RIGHT(COLUMN_NAME, LEN(COLUMN_NAME) - CHARINDEX('_', COLUMN_NAME)) + ''' AS ' + QUOTENAME(COLUMN_NAME + '_Number')
+SELECT @SelectSolutionNumbers = 
+STUFF((SELECT ', '''+ RIGHT(COLUMN_NAME, LEN(COLUMN_NAME) - CHARINDEX('_', COLUMN_NAME)) + ''' AS ' + QUOTENAME(COLUMN_NAME + '_Number')
     FROM INFORMATION_SCHEMA.COLUMNS
     WHERE TABLE_SCHEMA = 'dbo'
     AND TABLE_NAME = 'NICU_Modelling_Activities_Output'
@@ -27,13 +29,13 @@ Select Solution,
 		SolutionNumber,
 		CC_Level,
 		Count(*),
-		Case when CC_Activity_Date between ''2018-03-31'' and ''2019-04-01''
+		Case when CC_Activity_Date between ''2018-04-01'' and ''2019-03-31''
 				then ''18/19''
-			 when CC_Activity_Date between ''2019-03-31'' and ''2020-04-01''
+			 when CC_Activity_Date between ''2019-04-01'' and ''2020-03-31''
 				then ''19/20''
-			 when CC_Activity_Date between ''2020-03-31'' and ''2021-04-01''
+			 when CC_Activity_Date between ''2020-04-01'' and ''2021-03-31''
 				then ''20/21''
-			 when CC_Activity_Date between ''2021-03-31'' and ''2022-04-01''
+			 when CC_Activity_Date between ''2021-04-01'' and ''2022-03-31''
 				then ''21/22''
 			 end as Fin_Year
 		From (
@@ -54,6 +56,7 @@ Select Solution,
 					  ,' + @SolutionColumns + '
 					  ,' + @SelectSolutionNumbers + '
 				FROM [DCBI_Live].[dbo].[NICU_Modelling_Activities_Output]
+				where Latest = 1
 			) AS SourceTable UNPIVOT
 			(
 				Solution FOR SolutionColumn IN (' + @SolutionColumns + ')
@@ -68,17 +71,28 @@ Select Solution,
         Site,
 		SolutionNumber,
 		CC_Level,
-		Case when CC_Activity_Date between ''2018-03-31'' and ''2019-04-01''
+		Case when CC_Activity_Date between ''2018-04-01'' and ''2019-03-31''
 				then ''18/19''
-			 when CC_Activity_Date between ''2019-03-31'' and ''2020-04-01''
+			 when CC_Activity_Date between ''2019-04-01'' and ''2020-03-31''
 				then ''19/20''
-			 when CC_Activity_Date between ''2020-03-31'' and ''2021-04-01''
+			 when CC_Activity_Date between ''2020-04-01'' and ''2021-03-31''
 				then ''20/21''
-			 when CC_Activity_Date between ''2021-03-31'' and ''2022-04-01''
+			 when CC_Activity_Date between ''2021-04-01'' and ''2022-03-31''
 				then ''21/22''
 			 end 
+	ORDER BY SolutionNumber
+		,Solution
+		,CC_Level
+		,Case when CC_Activity_Date between ''2018-04-01'' and ''2019-03-31''
+				then ''18/19''
+			 when CC_Activity_Date between ''2019-04-01'' and ''2020-03-31''
+				then ''19/20''
+			 when CC_Activity_Date between ''2020-04-01'' and ''2021-03-31''
+				then ''20/21''
+			 when CC_Activity_Date between ''2021-04-01'' and ''2022-03-31''
+				then ''21/22''
+			 end'
 
-'
 
 PRINT @DynamicUnpivotQuery
 EXEC sp_executesql @DynamicUnpivotQuery
